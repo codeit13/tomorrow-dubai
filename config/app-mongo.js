4d367b732e20dabbc3525a9a64cedff1ad52f04e
Sumit Chauhan <cosmictunnel376@gmail.com>
2024-01-14 22:33:03 +0530

    Updated mongo

--- config/app-mongo.js
+++ config/app-mongo.js
@@ -3,7 +3,6 @@ const connect = () => {
   mongoose
     .connect("mongodb://127.0.0.1/tomorrowdubai", {
       useNewUrlParser: true,
-      useFindAndModify: false,
       useUnifiedTopology: true,
     })
     .then(() => {
