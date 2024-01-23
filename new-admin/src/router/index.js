c0640a3bd92dd4234749941c44325315eb425227
Sumit Chauhan <cosmictunnel376@gmail.com>
2024-01-23 12:09:50 +0530

    Added new-admin

--- new-admin/src/router/index.js
+++ new-admin/src/router/index.js
@@ -7,17 +7,17 @@ const routes = [
     component: () => import("@/views/Dashboard.vue"),
   },
   {
-    path: "/properties",
+    path: "/portal/properties",
     name: "Properties",
     component: () => import("@/views/Properties.vue"),
   },
   {
-    path: "/news",
+    path: "/portal/news",
     name: "News",
     component: () => import("@/views/News.vue"),
   },
   {
-    path: "/contact",
+    path: "/portal/contact",
     name: "Contact",
     component: () => import("@/views/Contact.vue"),
   },
--- new-admin/src/views/Contact.vue
+++ new-admin/src/views/Contact.vue
@@ -223,7 +223,7 @@
           <nav>
             <ol class="flex items-center gap-2">
               <li>
-                <a class="cursor-pointer" @click="$router.push('/')">
+                <a class="cursor-pointer" @click="$router.push('/portal')">
                   Dashboard /
                 </a>
               </li>
