6a6b9967e3a41e85c89a8640d48ed21e7f577e85
Sumit Chauhan <cosmictunnel376@gmail.com>
2024-02-06 13:13:59 +0530

    Added changes

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
+// const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: __dirname + "/dist",
-        // Required - Routes to render.
-        routes: ["/partner"],
-      }),
-    ],
-  },
+  // configureWebpack: {
+  //   plugins: [
+  //     new PrerenderSPAPlugin({
+  //       staticDir: __dirname + "/dist",
+  //       // Required - Routes to render.
+  //       routes: ["/partner"],
+  //     }),
+  //   ],
+  // },
 });
