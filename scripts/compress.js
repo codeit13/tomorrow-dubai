a3da2beaa669cbb94ee3f6edfcd76c3ed8dc3dff
Sumit Chauhan <cosmictunnel376@gmail.com>
2024-07-16 10:17:00 +0530

    Added compress script

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,31 +1,9 @@
 const mongoose = require("mongoose");
 const tinify = require("tinify");
 const fs = require("fs");
-const path = require("path");
-// const { firebase, storage } = require("./firebaseClientConfig");
 const { listingDB } = require("../model/listing");
 
 tinify.key = "P6LG99zZGvW7YfSzwZ7WcJsvST8GpqJT";
-
-const authenticateFirebase = async () => {
-  // Sign in anonymously (or use any other sign-in method)
-  await firebase.auth().signInAnonymously();
-};
-
-const uploadToFirebaseStorage = async (filePath, fileName) => {
-  const storageRef = storage.ref();
-  const fileRef = storageRef.child(`images/${fileName}`);
-  const fileData = fs.readFileSync(filePath);
-  const metadata = {
-    contentType: "image/png",
-    cacheControl: "public, max-age=31536000",
-  };
-
-  await fileRef.put(fileData, metadata);
-
-  const url = await fileRef.getDownloadURL();
-  return url;
-};
 
 (async () => {
   await mongoose.connect("mongodb://127.0.0.1/test", {
@@ -37,9 +15,6 @@ const uploadToFirebaseStorage = async (filePath, fileName) => {
 
   let json = {};
 
-  // await Promise.all(
-  // listings.map(async (listing) => {
-  //   return new Promise(async (r) => {
   for (const listing of listings) {
     const { _id, images } = listing;
 
@@ -49,16 +24,15 @@ const uploadToFirebaseStorage = async (filePath, fileName) => {
     json[_id] = [];
     await Promise.all(
       images.map(async (url, i) => {
-        console.log(i);
         return new Promise((resolve) => {
           try {
-            console.log("Compressing::", url);
+            console.log(i, ". Compressing::", url);
             tinify.fromUrl(url)._url.then((compressedUrl) => {
               json[_id].push(compressedUrl);
               resolve(compressedUrl);
             });
           } catch (error) {
-            console.log(error);
+            console.log(e.message);
             console.log("JSON:\n", json);
             json[_id].push(url);
             resolve("EMPTY");
@@ -67,12 +41,6 @@ const uploadToFirebaseStorage = async (filePath, fileName) => {
       })
     );
   }
-  // r();
-  // });
-
-  // await listingDB.updateOne({ _id }, { $set: { images: newImages } });
-  // })
-  // );
 
   console.log("\n\n\n\n", json);
 })();
